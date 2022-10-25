db0348da810c4e0d119514c4adf0f0ed89e0ef3d
Alexandre Baur <senolem22@gmail.com>
2022-10-25 16:13:41 +0200

    aa

--- srcs/ray_casting/hooks_2.c
+++ srcs/ray_casting/hooks_2.c
@@ -6,7 +6,7 @@
 /*   By: albaur <albaur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 15:53:14 by albaur            #+#    #+#             */
-/*   Updated: 2022/10/25 15:53:59 by albaur           ###   ########.fr       */
+/*   Updated: 2022/10/25 16:01:34 by albaur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ static void	ft_mouse_input_child(t_var var, t_ray *ray, t_data *data, double x)
 		ray->dir.y = var.old_dir_x * ray->mpsin + ray->dir.y * ray->mpcos;
 		var.old_plane_x = ray->plane.x;
 		ray->plane.x = ray->plane.x * ray->mpcos - ray->plane.y * ray->mpsin;
-		ray->plane.y = var.old_plane_x * ray->mpsin + ray->plane.y * ray->mpcos;
+		ray->plane.y = var.old_plane_x * ray->mpsin + ray->plane.y
+			* ray->mpcos;
 		data->refresh = 1;
 	}
 	data->mouse_x = x;
@@ -46,7 +47,8 @@ void	ft_mouse_input(double x, double y, void *param)
 		ray->dir.y = var.old_dir_x * ray->mnsin + ray->dir.y * ray->mncos;
 		var.old_plane_x = ray->plane.x;
 		ray->plane.x = ray->plane.x * ray->mncos - ray->plane.y * ray->mnsin;
-		ray->plane.y = var.old_plane_x * ray->mnsin + ray->plane.y * ray->mncos;
+		ray->plane.y = var.old_plane_x * ray->mnsin + ray->plane.y 
+			* ray->mncos;
 		data->refresh = 1;
 	}
 	ft_mouse_input_child(var, ray, data, x);
