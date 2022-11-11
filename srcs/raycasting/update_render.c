12975e20975f37fd80c7fb80eb9a60dfef3d4786
FateEvent <fabio.venturini.1989@gmail.com>
2022-11-11 17:43:59 +0100

    the doors disappear

--- srcs/raycasting/update_render.c
+++ srcs/raycasting/update_render.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:21:12 by albaur            #+#    #+#             */
-/*   Updated: 2022/11/11 14:15:58 by faventur         ###   ########.fr       */
+/*   Updated: 2022/11/11 17:42:43 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,9 +45,4 @@ void	update_render(void *param)
 	data->timer = get_time();
 	if (data->enemy.alive == 0)
 		show_death(data);
-	if (ray->door.sliding == 1)
-	{
-		ray->door.door_offsets[ray->map_pos.y][ray->map_pos.x] += 0.20;
-		ray->door.sliding = 0;
-	}
 }
